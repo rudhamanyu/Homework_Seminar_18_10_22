cdc18e402cf187e6fd1f535690642193ca9dab8e
RuslanGareev <rudhamanyu@gmail.com>
2022-10-22 01:29:00 +0500

    Common commit

---
+++ Homework1/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+string[] GetNumbers(string message)
+{
+    Console.WriteLine(message);
+    string[] array = (Console.ReadLine() ?? "").Split(new char[] { ',', ' ' });
+    return array;
+}
+int numbersGreaterThanZero(string[] array)
+{
+    int[] arr = new int[array.Length];
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = int.Parse(array[i]);
+        if (arr[i] > 0) count += 1;
+    }
+    return count;
+}
+string[] arr = GetNumbers("Введите числа через пробел или запятую: ");
+int result = numbersGreaterThanZero(arr);
+Console.WriteLine($"{result} - количество чисел больше нуля");
---
+++ Homework2/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+double GetNumber(string message)
+{
+    Console.Write(message);
+    double number = double.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+void IntersectionPoint(double b1, double k1, double b2, double k2)
+{
+    double x = ((b2 - b1) / (k1 - k2));
+    double y = k1 * x + b1;
+
+    if (k1 != k2)
+        Console.WriteLine($"Прямые пересекаются в точке ({x};{y})");
+    if (b1 == b2 && k1 == k2)
+        Console.WriteLine($"Прямые совпадают");
+    if (b1 != b2 && k1 == k2)
+        Console.WriteLine($"Прямые паралельны");
+}
+
+double b1 = GetNumber("Введите значение b1: ");
+double k1 = GetNumber("Введите значение k1: ");
+double b2 = GetNumber("Введите значение b2: ");
+double k2 = GetNumber("Введите значение k2: ");
+IntersectionPoint(b1, k1, b2, k2);
